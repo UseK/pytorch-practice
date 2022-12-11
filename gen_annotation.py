4bc03391ba847eaa14b930b404ff62757be92562
UseK <use.k.james@gmail.com>
2022-12-11 13:17:27 +0900

    Can gen annotation with python gen_annotation.py > data/mydata/annotation.csv

---
+++ gen_annotation.py
@@ -0,0 +1,19 @@
+import datetime
+
+remain = datetime.timedelta(minutes=2, seconds=25)
+# 1 ~ 3.jpg -> 2:25
+# 4 ~ 33.jpg -> 2:24
+# 34 ~ 63.jpg -> 2:23
+# 290 ~ 894.jpg -> finish_battle
+first_time_change = 4
+frame_rate = 30
+time_to_finish = 290
+
+for i in range(1, 894+1):
+    if (i - first_time_change) % frame_rate == 0:
+        # print(f"{i} remain time change!")
+        remain -= datetime.timedelta(seconds=1)
+    image_path = f"{str(i).zfill(6)}.jpg"
+    label = "finish" if i >= time_to_finish else remain
+    print(f"{image_path},{label}")
+
